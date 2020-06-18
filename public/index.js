a14d5a121d828526f8839900cb274205abbdedba
Manas Mhasakar <manasmhasakar2001@gmail.com>
2020-06-18 12:55:18 +0530

    Add files via upload

--- public/index.js
+++ public/index.js
@@ -1,37 +1,72 @@
 async function asubmit(){
 
-  const name = $('#nameID').val();
-  const email = $('#emailID').val();
-  const leaderID = $('#leaderID').val();
-  const leaderphID = $('#leaderphID').val();
-  const otherMentorID = $('#otherMentorID').val(); 
-  const subjects = $('#subjectID').find(":selected").text();
-  const gradesID = $('#gradesID').val();
-  const ratioID = $('#ratioID').val();
+// const validationFunctions = require('./validationFunctions');
 
-  const data = {
-      name,
-      email,
-      leaderID,
-      leaderphID,
-      otherMentorID,
-      subjects,
-      gradesID,
-      ratioID
-  }
+const name = $('#nameID').val();
+const email = $('#emailID').val();
+const leaderID = $('#leaderID').val();
+const leaderphID = $('#leaderphID').val();
+const otherMentorID = $('#otherMentorID').val(); 
+const subjects = $('#subjectID').find(":selected").text();
+const gradesID = $('#gradesID').val();
+const ratioID = $('#ratioID').val();
 
-  console.log(data)
+const obj = {
+    name,
+    email,
+    leaderID,
+    leaderphID,
+    otherMentorID,
+    subjects,
+    gradesID,
+    ratioID
+}
 
-  const options = {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-  }
+// let errorObject={};
 
-  const api = await fetch('/api',options)
-  const res = await api.json();
-  console.log("done",res)
-  alert("data recorded")
+// if(!validationFunctions.nameCheck(obj.name))
+// {
+//   errorObject.name = errors.NAME;
+// }
+
+// if(!validationFunctions.emailCheck(obj.email))
+// {
+//   errorObject.email = errors.EMAIL;
+// }
+
+// if(!validationFunctions.mentoridCheck(obj.leaderID))
+// {
+//   errorObject.id = errors.ID;
+// }
+
+// if(!validationFunctions.phoneCheck(obj.leaderphID))
+// {
+//   errorObject.phone = errors.PHONE;
+// }
+
+// if(!validationFunctions.mentoridCheck(obj.otherMentorID))
+// {
+//   errorObject.id = errors.ID;
+// }
+  
+// if (!validationFunctions.isEmpty(errorObject)) {
+//   console.log(errorObject);
+// } 
+// else 
+{
+console.log(obj);
+}
+
+const options = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(obj)
+}
+
+const api = await fetch('/api',options)
+const res = await api.json();
+console.log("done",res)
+alert("data recorded")
 }
